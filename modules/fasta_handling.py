686e8c1bf6e74f549def6fa3d16ab33c2a9a4295
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2023-12-07 18:15:02 +1000

    Reimplement FastaCollection; small BINge mods
    Added two flags to skip some steps (mostly just so I can benchmark them), improved output file name handling, skip some unnecessary self linking (at least, I think it's unnecessary) and the code is pretty much functional once again with all the changes.

--- modules/fasta_handling.py
+++ modules/fasta_handling.py
@@ -1,5 +1,6 @@
 import os, sys, re
 from Bio import SeqIO
+from pyfaidx import Fasta
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from Various_scripts.Function_packages import ZS_SeqIO # expose this to any callers
@@ -125,3 +126,46 @@ class AnnotationExtractor:
             
             # Yield products
             yield mrnaID, exonSeq, cdsSeq
+
+class FastaCollection:
+    '''
+    Wrapper for pyfaidx Fasta objects which allows multiple to be combined
+    and queried as one logical entity.
+    
+    Parameters:
+        fastaFiles -- a list of strings pointing to the locations of FASTA files
+                      which are to be loaded in using pyfaidx.Fasta
+    '''
+    def __init__(self, fastaFiles):
+        self.fastaFiles = fastaFiles
+        self.records = []
+        
+        self._parse_fastas()
+    
+    def _parse_fastas(self):
+        for fastaFile in self.fastaFiles:
+            self.records.append(Fasta(fastaFile))
+    
+    def __getitem__(self, key):
+        for records in self.records:
+            try:
+                return records[key]
+            except:
+                pass
+        raise KeyError(f"'{key}' not found in collection")
+    
+    def __contains__(self, key):
+        try:
+            self[key] # __getitem__ raises exception if the key isn't found
+            return True
+        except:
+            return False
+    
+    def __iter__(self):
+        for records in self.records:
+            yield from records
+    
+    def __repr__(self):
+        return (f"<FastaCollection object;num_records='{len(self.records)}';" +
+                f"fastaFiles={self.fastaFiles}"
+        )
