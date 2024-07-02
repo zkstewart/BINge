d75a790951ebfaeb9c9126b14f66bf668ed5c9b9
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2024-07-02 12:14:45 +1000

    generate_annotation use ZS_GO
    These functions got added into a shared codebase so let's just import that in case any fixes happen down the line

--- utilities/generate_annotation_table.py
+++ utilities/generate_annotation_table.py
@@ -11,7 +11,7 @@ from goatools import obo_parser
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from Various_scripts.Function_packages import ZS_SeqIO
+from Various_scripts.Function_packages import ZS_SeqIO, ZS_GO
 
 # Define functions
 def validate_args(args):
@@ -277,7 +277,7 @@ def update_table_with_gos(originalTable, newTable, hitMapDict, goObo):
                         bestHitGOs = [ bhg for bhg in bestHitGOs if bhg != "" and bhg != "." ]
                         
                         # Try to fix any obsoletions (if any)
-                        fixedGOs = fix_obsoletions(bestHitGOs, goObo, queriedGOs)
+                        fixedGOs = ZS_GO.fix_obsoletions(bestHitGOs, goObo, queriedGOs)
                         
                         # If we ended up with no hits, handle that
                         if fixedGOs == []:
@@ -298,103 +298,6 @@ def update_table_with_gos(originalTable, newTable, hitMapDict, goObo):
                 
                 # Write to file
                 fileOut.write("{0}\t{1}\n".format("\t".join(sl), "\t".join(outputValues)))
-
-def fix_obsoletions(goList, goObo, queriedGOs):
-    '''
-    Helper function to receive a list of GO terms, alongside the parsed go.obo
-    and a dictionary potentially containing previous API queries. Using this, it
-    will check if the GOs in the GO list are obsolete and, if so, attempt to fix
-    them via API query.
-    
-    Parameters:
-        goList -- a list of string GO terms e.g., ['GO:0033644', 'GO:0016020'].
-        goObo -- a obo_parser.GODag object which has parsed a go.obo file.
-        queriedGOs -- a dictionary with previous API query hits (if any) that
-                      will be modified by this function; has structure like:
-                      {
-                          'goID1': ['replacementID1'],
-                          'goID2': ['replacementID1', 'replacementID2'],
-                          'goID3': None, # if no replacement is available
-                          ...
-                      }
-    '''
-    fixedGOs = []
-    for goTerm in goList:
-        
-        # If we have a hit, everything's good!
-        if goTerm in goObo:
-            fixedGOs.append(goTerm)
-        
-        # Otherwise, if we found an obsoletion, handle it
-        else:
-            # Perform an API query if we haven't seen this GO yet
-            if not goTerm in queriedGOs:
-                replacements = query_go_api(goTerm)
-                queriedGOs[goTerm] = replacements
-            
-            # Get the API query result
-            apiGOs = queriedGOs[goTerm]
-            
-            # Store result if we were successful
-            if apiGOs != None:
-                fixedGOs += apiGOs
-    return fixedGOs
-
-def query_go_api(goTerm):
-    '''
-    Parameters:
-        goTerm -- a GO term to query for replacement to its obsoletion
-    Returns:
-        replacementList -- a list containing GO terms which replace this one, OR
-                           None if no replacements were found
-    '''
-    # Query the API
-    apiUrl = "https://www.ebi.ac.uk/QuickGO/services/ontology/go/terms/GO%3A{0}/history".format(
-        goTerm.split(":")[1]
-    )
-    response = requests.get(apiUrl)
-    json = response.json()
-    
-    # Handle errors
-    if "messages" in json:
-        message = json["messages"][0]
-        print(f"Querying the GO term '{goTerm}' resulted in an API error")
-        print(f"The return message was: '{message}'")
-        print("The program will continue, but this error means no result will be " + 
-              "returned for this GO term")
-        return None
-    resultsDict = json["results"][0]
-    
-    # Handle situations where the GO isn't actually obsolete
-    if resultsDict["isObsolete"] != True:
-        print(f"The GO term '{goTerm}' does not appear to be obsolete")
-        print("However, it is not found in the go.obo file?")
-        print("The program will continue, but this error means no ancestor terms " + 
-              "will be returned for this GO term")
-        return [goTerm]
-    
-    # Process results and try to find replacements or considers
-    else:
-        replacedBys = set()
-        considers = set()
-        for historyDict in resultsDict["history"]:
-            historyText = historyDict["text"]
-            if "consider GO:" in historyText:
-                considerMatch = re.search(r"consider (GO:\d{7})", historyText)
-                if considerMatch != None:
-                    considers.add(considerMatch.group(1))
-            if "replaced_by GO:" in historyText:
-                replacedByMatch = re.search(r"replaced_by (GO:\d{7})", historyText)
-                if replacedByMatch != None:
-                    replacedBys.add(replacedByMatch.group(1))
-    
-    # Return the best value, or None if no replacements were possible
-    if len(replacedBys) > 0:
-        return list(replacedBys)
-    elif len(considers) > 0:
-        return list(considers)
-    else:
-        return None
 
 def update_table_with_seq_details(originalTable, newTable, hitMapDict, 
                                   representativesFasta, blastFasta):
