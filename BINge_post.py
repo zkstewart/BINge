66d322f4343901133c1cd3904c5ebce7b6dff34a
Zachary Kenneth Stewart <pro_zac@hotmail.com>
2025-03-31 20:30:29 +1000

    BINge_post blast avoid printing superfluous
    Setting .isSetup will avoid unneeded print statements of 'skipping ...'.

--- BINge_post.py
+++ BINge_post.py
@@ -690,6 +690,7 @@ def bmain(args, locations):
     mms2 = ZS_BlastIO.MMseqs(queryDB, targetDB, os.path.dirname(args.mms2Exe), tmpDir)
     mms2.threads = args.threads
     mms2.evalue = 1 # weak threshold, leave it to 'filter' to decide on a good one
+    mms2.isSetup = True
     mms2.mmseqs(outputFileName, force=True) # force=True to overwrite any existing file if '.ok' is missing
     touch_ok(outputFileName)
     
